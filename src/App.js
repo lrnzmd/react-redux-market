20cbd01eb86d92ab53f4a47314c0d888eee01912
lorenzo amodeo <lrnzmd@gmail>
2020-09-20 16:47:10 +0200

    filter mission complite

--- src/App.js
+++ src/App.js
@@ -14,11 +14,27 @@ class App extends React.Component {
   }
 
   sortProducts = (event) => {
-    console.log(event.target.value)
-  }
+    const sort = event.target.value
+    this.setState((state)=>({
+      sort: sort ,
+      products: this.state.products.slice().sort((a,b) => 
+      sort === 'lowest'? a.price>b.price ? 1:-1 :
+      sort === 'highest'? a.price<b.price ? 1:-1 :
+      a._id<b._id ? 1:-1
+      ),
+    }));
+  };
 
   filterProducts = (event) => {
-    console.log(event.target.value)
+    if(event.target.value === ""){
+      this.setState({size: event.target.value, products:data.products})
+    }
+    else{
+      this.setState({
+      size: event.target.value,
+      products: data.products.filter(product => product.availableSizes.indexOf(event.target.value)>=0)
+    })
+    }
   }
 
   render() {
