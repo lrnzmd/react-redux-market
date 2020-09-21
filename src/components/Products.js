9d29feb62342f4e0437e5a6896da5bc8b750ca75
lorenzo amodeo <lrnzmd@gmail>
2020-09-21 12:10:03 +0200

    fade reacrt-reveal

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import formatCurrency from "../util";
+import Fade from 'react-reveal/Fade';
 
 
 export default class Products extends Component {
     render() {
         return (
             <div>
+                <Fade bottom cascade={true}>
                 <ul className="products">
                 {this.props.products.map((product) =>(
                     <li key={product._id}>
@@ -29,6 +31,7 @@ export default class Products extends Component {
                     </li>
                     ))}
                 </ul>
+                </Fade>
             </div>
         )
     }
